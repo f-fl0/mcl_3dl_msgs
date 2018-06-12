48a6e4ad7aa09eb26e85c915b3d80aa8e4f51aaa
Atsushi Watanabe <atsushi.w@ieee.org>
2018-06-12 14:24:22 +0900

    Initial drop from mcl_3dl package

---
+++ .travis/test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -o errexit
+set -o verbose
+
+pip install gh-pr-comment
+
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+cd /catkin_ws
+
+sed -i -e '5a set(CMAKE_C_FLAGS "-Wall -Werror")' \
+  /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
+sed -i -e '5a set(CMAKE_CXX_FLAGS "-Wall -Werror")' \
+  /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
+
+CM_OPTIONS=""
+
+catkin_make ${CM_OPTIONS} || \
+  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
+catkin_make tests -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
+  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
+catkin_make run_tests -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
+  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
+
+if [ catkin_test_results ];
+then
+  result_text="
+\`\`\`
+`catkin_test_results --all | grep -v Skipping || true`
+\`\`\`
+"
+else
+  result_text="
+\`\`\`
+`catkin_test_results --all | grep -v Skipping || true`
+\`\`\`
+`find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;'`
+"
+fi
+catkin_test_results || (gh-pr-comment "FAILED on ${ROS_DISTRO}" "Test failed$result_text"; false)
+
+gh-pr-comment "PASSED on ${ROS_DISTRO}" "All tests passed$result_text"
+
+cd ..
+rm -rf /catkin_ws || true
